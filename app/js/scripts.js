493d7bbe890b6bcbfc1c25b573e5ebc55c8e23b2
Igor Buksha <ibuksha@splitdev.pro>
2022-05-25 13:50:40 +0300

    IB

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,3 +1,18 @@
-$(document).ready(function () {
+document.addEventListener('DOMContentLoaded', function () {
 
-})
+  // accordion
+  let acc = document.querySelectorAll('.accordion__item_button');
+  for (let i = 0; i < acc.length; i++) {
+    acc[i].addEventListener('click', function () {
+      this.classList.toggle('active');
+      let panel = this.nextElementSibling;
+      if (panel.classList.contains('active')) {
+        panel.classList.remove('active');
+        panel.style.maxHeight = 0 + 'px';
+      } else {
+        panel.classList.add('active');
+        panel.style.maxHeight = panel.scrollHeight + 'px';
+      }
+    });
+  }
+});
